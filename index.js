31f5492c86a603e5ddb1266ce5acda9fef34b67b
Jonathan <jpreuven@gmail.com>
2023-05-03 02:08:50 -0700

    YONI - objects lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "George",
+    streetAddress: "11 Broadway"
+}
+
+const updateEmployeeWithKeyAndValue = function(employee, key, value) {
+    const newObject = {
+        ...employee, 
+        [key]: value,
+    }
+    return newObject
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = function(employee,key,value) {
+    employee[key] = value;
+    return employee
+}
+
+const deleteFromEmployeeByKey = function(employee,key,value) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = function(employee,key,value) {
+    delete employee[key];
+    return employee;
+}
